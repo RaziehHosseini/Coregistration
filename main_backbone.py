a50b391cc32028cb1852a0d0c4ef7660c826d090
Razieh Hosseini <hosseini.rz@outlook.com>
2019-05-24 19:03:26 +0200

    add one file

--- main_backbone.py
+++ main_backbone.py
@@ -10,11 +10,16 @@ import numpy as np
 import scipy.io as spio
 import scipy.signal as spsig
 from matplotlib import cm
-from zeropad2d import zeropad2d
 import imageio
 from skimage import io
-import point_detectors
+                                                                               # TODO adding path to directory in read_img
+                                                                               # working with relative directory (to add module from different directory)
+import sys
+sys.path.append('./bau')
+import main_backbone_bau
 
+import point_detectors
+import match_points
 
 def read_img(data1, key1, data2, key2):
 
@@ -42,6 +47,7 @@ def read_img(data1, key1, data2, key2):
     slv_amp_se = np.clip(slv_amp_se, 0, 2)
     
     # Save image as tiff format
+
     imageio.imsave('./ori_data/master.tiff', mstr_amp_se)
     imageio.imsave('./ori_data/slave.tiff', slv_amp_se)
     
@@ -70,8 +76,9 @@ def read_img_nocrop(data1, key1, data2, key2):
     slv_amp_se = np.clip(slv_amp_se, 0, 2)
     
     # Save image as tiff format
-    imageio.imsave('./noiseless_data/master.tiff', mstr_amp_se)
-    imageio.imsave('./noiseless_data/slave.tiff', slv_amp_se)
+    
+    imageio.imsave('./kerman_data/master.tiff', mstr_amp_se)
+    imageio.imsave('./kerman_data/slave.tiff', slv_amp_se)
     
     return mstr_amp_se, slv_amp_se
     
@@ -90,7 +97,7 @@ def display(mstr_amp, slv_amp):
     
     ### Slave
     fig = plt.figure('slave')
-    ax = fig.add_subplot(112)
+    ax = fig.add_subplot(111)
     cax = ax.imshow(slv_amp, interpolation='nearest', cmap=cm.gray)
     ax.set_xlabel('Range')
     ax.set_ylabel('Azimuth')
@@ -102,39 +109,56 @@ def display(mstr_amp, slv_amp):
 
 
 if __name__=='__main__':
+      
 #    MASTER_FILE = './ori_data/master.mat'
 #    KEY1 = 'master'
 #    SLAVE_FILE = './ori_data/slave.mat'
 #    KEY2 = 'slave'
     
     # Plot the matlab data as ouput
-    MASTER_FILE = './noiseless_data/master.mat'
-    KEY1 = 'master0'
-    SLAVE_FILE = './noiseless_data/slave.mat'
-    KEY2 = 'slave0'
+#    MASTER_FILE = './noiseless_data/master.mat'
+#    KEY1 = 'master0'
+#    SLAVE_FILE = './noiseless_data/slave.mat'
+#    KEY2 = 'slave0'
+    
+    # Kerman data
+    MASTER_FILE = './kerman_data/master_crop.mat'
+    KEY1 = 'master_crop'
+    SLAVE_FILE = './kerman_data/slave_crop.mat'
+    KEY2 = 'slave_crop'
     
     [mstr_amp, slv_amp] = read_img_nocrop(MASTER_FILE, KEY1, SLAVE_FILE, KEY2)
-    display(mstr_amp, slv_amp)
+#    display(mstr_amp, slv_amp)
     
     # Read tiff image as input
-    my_img_master = io.imread('./noiseless_data/master.tiff')
-    my_img_slave = io.imread('./noiseless_data/slave.tiff')
-    
+    my_img_master = io.imread('./kerman_data/master.tiff')
+    my_img_slave = io.imread('./kerman_data/slave.tiff')
+    display (my_img_master, my_img_slave)
+
+
+    """================================ point detection============================
+    ==============================================================================="""
 #   # Implement harris corner detector
-#   point_detectors.harris_skimage(my_img_master)
-#   point_detectors.harris_skimage(my_img_slave)
+#   master_points,_ = point_detectors.harris_skimage(my_img_master)
+#   slave_points,_ = point_detectors.harris_skimage(my_img_slave)
     
     
 #    # Implement foerstner detector
-#    point_detectors.foerstner_skimage(my_img_master)
-#    point_detectors.foerstner_skimage(my_img_slave)  
+    master_points,_ = point_detectors.foerstner_skimage(my_img_master)
+    slave_points,_ = point_detectors.foerstner_skimage(my_img_slave)  
 
-#   # Implement fast corner detector
-#   point_detectors.fast_skimage(my_img_master)
-#   point_detectors.fast_skimage(my_img_slave)  
+   # Implement fast corner detector
+#    master_points,_ = point_detectors.fast_skimage(my_img_master)
+#    slave_points,_ = point_detectors.fast_skimage(my_img_slave)
+#   
 	
-    # Harris tiled
-    point_detectors.tiled_point_detection(my_img_master, 6, method = "shi_tomasi_skimage")
-
+    # Tiled matching
+     #master_points,_ = point_detectors.tiled_point_detection(my_img_master, 6, method = "kitchen_rosenfeld_skimage")
+     #slave_points,_ = point_detectors.tiled_point_detection(my_img_slave, 6, method = "kitchen_rosenfeld_skimage")
+    
+    """================================ Matching ============================
+    =============================================================================="""
+    matcher = match_points.Matcher(my_img_master,  my_img_slave, master_points, slave_points)
+    src, dest = matcher.match()
     
     
